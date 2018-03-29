2a99dccc43812f3a269949074f5719f15898c122
PDC9WBNEZNC5Q7C\Administrator <794529075@qq.com>
2018-03-29 22:36:29 +0800

    2018年3月29日22:36:23

--- app/src/main/java/com/xatu/yuexin/welding4sl/activity/InputInfoActivity.java
+++ app/src/main/java/com/xatu/yuexin/welding4sl/activity/InputInfoActivity.java
@@ -6,6 +6,7 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import com.xatu.yuexin.welding4sl.R;
@@ -21,6 +22,7 @@ public class InputInfoActivity extends AppCompatActivity {
     private EditText projectAdminNum;//工程管理员编号
     private EditText constructionNum;//施工企业编号
     private EditText managerNum;//管理编号
+    private Button inputFinish;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -35,18 +37,11 @@ public class InputInfoActivity extends AppCompatActivity {
         projectAdminNum = (EditText) findViewById(R.id.projectAdminNum);//工程管理员编号
         constructionNum = (EditText) findViewById(R.id.constructionNum);//施工企业编号
         managerNum = (EditText) findViewById(R.id.managerNum);//管理编号
+        inputFinish = (Button)findViewById(R.id.inputFinish);
 
-//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-//        setSupportActionBar(toolbar);
-//
-//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-//        fab.setOnClickListener(new View.OnClickListener() {
-//            @Override
-//            public void onClick(View view) {
-//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-//                        .setAction("Action", null).show();
-//            }
-//        });
+
     }
+
+
 
 }
